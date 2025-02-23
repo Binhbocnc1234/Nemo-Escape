62a1b81271e08c534c50034901ae8c73770ba7ef
Leemoqua <94879197+Leemoqua@users.noreply.github.com>
2025-02-23 12:07:34 +0700

    WaveManager update
    ok

--- Nemo Escape-Project/Assets/Scripts/WaveManager/Wave.cs
+++ Nemo Escape-Project/Assets/Scripts/WaveManager/Wave.cs
@@ -10,9 +10,8 @@ public class Wave : MonoBehaviour{
 
     int[] chance = new int[4] {10, 20, 30, 40};
     
-
-
-    protected List<GameObject> level1 = new List<GameObject>();
+    
+    public List<GameObject> level1 = new List<GameObject>();
     protected List<GameObject> level2 = new List<GameObject>();
     protected List<GameObject> level3 = new List<GameObject>();
 
@@ -21,6 +20,9 @@ public class Wave : MonoBehaviour{
 
     // Field
     public int level;
+    public float time_count = 1f;
+    public GameObject container;
+    Timer timer = new Timer(1f);
     
     // When you click choose level button, it'll call singleton to change WaveLevel
     [HideInInspector] public int waveLevel = 1;
@@ -40,7 +42,26 @@ public class Wave : MonoBehaviour{
           
     }
 
+    void Add(string s, List<GameObject> list_entity){
+        Transform child = container.transform.Find(s); 
+
+        foreach (Transform c in child){
+            list_entity.Add(c.gameObject);
+        }
+
+    }
     public void Start(){
+
+        
+        Add("Level1", level1);
+        // Add("Level2", level2);
+        // Add("Level3", level3);
+
+       
+
+    }
+
+    public void Update(){
 
         Spawn();
 
@@ -68,13 +89,13 @@ public class Wave : MonoBehaviour{
 
     
     protected void Spawn(){
-
+        
         switch(waveLevel){
             // Fish tank
             case 1:
-                Timer timer = new Timer(1f);
+                
                 RandomModule randoms = new RandomModule();
-                while(Player.Instance.level < 5){
+                if(Player.Instance.level < 5){
                     if(timer.Count()){
                         Debug.Log("Spawn");
                         GameObject random = GetRandomObject(level1);
@@ -84,12 +105,14 @@ public class Wave : MonoBehaviour{
                             float random_Y = randoms.RandomBetween(instance.minBounds.y, instance.maxBounds.y);
                             Vector3 pos = new Vector3(instance.minBounds.x, random_Y, 0f);
                             GameObject clone = Instantiate(random, pos, Quaternion.identity);
+                            clone.GetComponent<Fish>().SetMainDirection(true);
                         }
                         else{
                             var instance = GameManager.Instance; 
                             float random_Y = randoms.RandomBetween(instance.minBounds.y, instance.maxBounds.y);
                             Vector3 pos = new Vector3(instance.maxBounds.x, random_Y, 0f);
                             GameObject clone = Instantiate(random, pos, Quaternion.identity);
+                            clone.GetComponent<Fish>().SetMainDirection(false);
                         }
 
                     }
